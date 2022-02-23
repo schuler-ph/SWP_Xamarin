abc20345c4f45ec9cfaaab34388ccbca8e291164
Philipp Schuler <schulerp03@gmail.com>
2022-02-23 14:59:00 +0100

    Finished fetching free rooms

--- SWP_Xamarin_Hotel/SWP_Xamarin_Hotel_API/Controllers/RoomsController.cs
+++ SWP_Xamarin_Hotel/SWP_Xamarin_Hotel_API/Controllers/RoomsController.cs
@@ -14,16 +14,36 @@ namespace SWP_Xamarin_Hotel_API.Controllers
         private HotelContext _context = new HotelContext();
 
         [HttpGet]
-        public List<Bills_Rooms> GetRooms()
+        public List<Room> GetRooms()
         {
-            // return this._context.Rooms.ToList();
-            return this._context.Bills_Rooms.Include("Room").ToList();
+            return this._context.Rooms.ToList();
         }
-        /*
+        
         [Route("freeRooms")]
         [HttpGet]
-        public List<Bills_Rooms> GetFreeRooms()
+        public List<Room> GetFreeRooms()
         {
-        }*/
+            List<Room> rooms = this._context.Rooms.ToList();
+            List<Bills_Rooms> bills_rooms = this._context.Bills_Rooms.ToList();
+
+            List<Room> freeRooms = new List<Room>();
+            DateTime currentDate = DateTime.Now;
+            bool roomFree;
+
+            foreach(Room r in rooms)
+            {
+                roomFree = true;
+
+                foreach(Bills_Rooms br in bills_rooms)
+                {
+                    if (br.Room.RoomId != r.RoomId) continue;
+                    if (br.BeginReservation < currentDate && currentDate < br.EndReservation) roomFree = false;
+                }
+
+                if (roomFree) freeRooms.Add(r);
+            }
+
+            return freeRooms;
+        }
     }
 }
