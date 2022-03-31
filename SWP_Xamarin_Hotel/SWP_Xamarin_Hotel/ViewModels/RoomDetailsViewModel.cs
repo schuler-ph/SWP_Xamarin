17186b54c2583c0e8839f01ac145656ec0a0fdef
Philipp Schuler <schulerp03@gmail.com>
2022-03-31 16:09:24 +0200

    added roomdetails

---
+++ SWP_Xamarin_Hotel/SWP_Xamarin_Hotel/ViewModels/RoomDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using SWP_Xamarin_Hotel.Models;
+using SWP_Xamarin_Hotel.Models.Common;
+
+namespace SWP_Xamarin_Hotel.ViewModels
+{
+    internal class RoomDetailsViewModel : BindingBase
+    {
+        private Room _room;
+        public Room Room
+        {
+            get { return _room; }
+            set
+            {
+                _room = value;
+                this.RaisePropertyChanged(nameof(Room));
+            }
+        }
+
+
+    }
+}
---
+++ SWP_Xamarin_Hotel/SWP_Xamarin_Hotel/Views/RoomDetails.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SWP_Xamarin_Hotel.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RoomDetails : ContentPage
+    {
+        public RoomDetails(Room room)
+        {
+            InitializeComponent();
+        }
+    }
+}
